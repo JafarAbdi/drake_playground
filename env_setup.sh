2c2d18dc50d20ee80dc9b825340db7b4e6543129
JafarAbdi <jafar.uruc@gmail.com>
2024-01-05 16:01:21 +0000

    Update setup

--- env_setup.sh
+++ env_setup.sh
@@ -2,4 +2,4 @@
 
 # Get the path to the directory containing this script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export PYTHONPATH=$PYTHONPATH:$SCRIPT_DIR/manipulation
+export PYTHONPATH=$PYTHONPATH:$SCRIPT_DIR/manipulation:$SCRIPT_DIR/underactuated
